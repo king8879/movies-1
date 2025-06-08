7892899e68af2f9f0d73e7f5aabbe76592c1109a
king8879 <k4507244@gmail.com>
2025-06-08 23:59:27 +0530

    Update info.py

--- info.py
+++ info.py
@@ -20,7 +20,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', '7053311287:AAHLgmUL2KINNb50qA5X1Svmnt2a0ss
 #---------------------------------------------------------------
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6554343173').split()]
 USERNAME = environ.get('USERNAME', "Donking88_bot") # ADMIN USERNAME
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002677298480,-1002445585006'))
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002677298480,'))
 MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'http://t.me/Donking88_bot')
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002806554507').split()]
 #---------------------------------------------------------------
